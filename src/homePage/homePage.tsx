0773115f496453a70e028201446a48884bced9c1
Christopher Aragon <christopher.aragon2018@gmail.com>
2023-10-08 07:21:52 -0400

    Update homePage.tsx
    Added a button waiting for "Add your own" functionality

--- src/homePage/homePage.tsx
+++ src/homePage/homePage.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './homePage.css'
 
 const activities = [
@@ -56,6 +57,7 @@ function HomePage() {
     return (
         <>
             <div className="bg-neutral-focus">
+                <Link to="/homePage" role="button" className="btn btn-accent text-black">Add your own</Link>
                 <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                     <h2 className="sr-only">Activities</h2>
 
